dd6bcbe885cfc518143d2778697831dbdfccd710
abdulrahmanbayo <ramhziworld@gmail.com>
2022-11-07 21:02:36 +0100

    Initial Commit

---
+++ profileapp/profileappApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileappApp.swift
+//  profileapp
+//
+//  Created by Kole-ibrahim Abdul rahman on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct profileappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
